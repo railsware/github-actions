07e49a3f08d2a4d68722bb5ff981d1b72212bb17
Igor Dobryn <igor.dobryn@brightbytes.net>
2020-10-06 12:55:18 +0300

    Proper output param name

--- read-from-ssm-param-store/index.js
+++ read-from-ssm-param-store/index.js
@@ -7,7 +7,7 @@ async function run() {
     const path = core.getInput("path", { required: true });
     const result = await readFromParamStore(path, region);
 
-    core.setOutput('result', result);
+    core.setOutput('param_json', result);
   } catch (error) {
     core.setFailed(error.message);
   }
